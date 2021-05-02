832e1b93f075b1f446fe0a7845f3d3b25291d4fe
Paul Dale <ldpercy@gmail.com>
2021-05-02 14:56:11 +1000

    setup for rss-parser and feed params

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const Parser = require('rss-parser');
+const parser = new Parser();
+const rssUrl = 'https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss';
+const episodeCount = 10;
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let result = 'Hello World!';
+  res.send(result);
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+
